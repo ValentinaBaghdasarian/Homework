82fd6f38cb84875716dbef20366134340542313a
vali <valbaghdassarian@gmail.com>
2022-08-20 14:22:13 +0400

    task

---
+++ das4/index.js
@@ -0,0 +1,42 @@
+// let newArray;
+// let helloWorld;
+// let getElement;
+// let newVariable;
+// let inArmenia;
+// let newString;
+// let userName;
+
+
+let nam = 4;
+{
+    nam = 5;
+    console.log(num)
+}
+    console.log(num)
+
+{
+    let y = 6;
+}
+console.log(y)
+
+
+let tea = 'sweet';
+let i = 5;
+
+if (i < 7) {
+ let tea = 'sour';
+   tea = 'iced';
+   i++
+}
+
+console.log(tea);
+
+
+let a = 7;
+let b = 9;
+
+function addThem() {
+  console.log(a + b);
+}
+
+addThem();
---
+++ das5/index.js
@@ -0,0 +1,13 @@
+const arr = ["Միանգամից", "Անկեղծ ասած", "Մարդ էլ կա, մարդ էլ", "Դու"];
+const author = {
+name: "Paruyr",
+surname: "Sevak",
+age: "47",
+address: "Zangakatun",
+favoritePoetry : function(val){
+  console.log(val);
+}
+};
+author.favoritePoetry(arr[2])
+
+
