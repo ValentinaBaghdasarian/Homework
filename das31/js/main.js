5c069f80693ff2ea30764558e93b7b0cd3c0a465
vali <valbaghdassarian@gmail.com>
2022-08-23 14:50:53 +0400

    film

---
+++ das31/js/main.js
@@ -0,0 +1,92 @@
+// "use strict";
+
+const poster = document.querySelector("header img");
+const advs = document.querySelectorAll("#main_promo .mp");
+const filmsBlock = document.getElementById("films");
+const form = document.querySelector("#add");
+
+const _DB = {
+	movies: [
+		"Logan", "Spider-Man", "The Seven Samurai",
+		"Bonnie and Clyde","Reservoir Dogs", "Crid",
+		"Doctor Zhivago", "The Deer Hunter", "Rocky"
+	]
+};
+
+function init () {
+	advs.forEach(adv => adv.remove());
+	poster.src = "img/bg2.jpg";
+	if (poster.src.slice(31) === "bg1.jpg") {
+		poster.alt = "GEMINI MAN";
+	} else {
+		poster.alt = "Hitman's wife's bodyguard";
+	}
+	document.title = poster.alt;
+}
+init();
+
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	let val = e.target.firstElementChild.value.trim();
+    val = val[0].toUpperCase() + val.slice(1).toLowerCase();
+	const check = document.querySelector("input[name='favorite']");
+
+	if (val !== "" && val !== "<" && val !== ">" && val !== "/" && val !== "?") {
+		if (check.checked) {
+            _DB.movies.push({filmName: val, check: true});
+			console.log(`This film <${val}> added to favorite`);
+		} _DB.movies.push({filmName: val, check: false});
+		_DB.movies.push(val);
+	}
+
+	setSort(_DB.movies);
+	createFilmsList(_DB.movies, filmsBlock);
+	e.target.reset();
+});
+
+function setSort(arr) {
+	arr.sort();
+}
+function createFilmsList (filmsArr, parent) {
+	parent.innerHTML = "";
+        setSort(filmsArr);
+        filmsArr.forEach((film, index) => {
+            if(typeof(film) === "object" && film.check === true){
+                setSort(filmsArr);
+            parent.innerHTML += `
+                <p style = "color: blue">
+                    ${index + 1}. 
+                    ${film.filmName.length >= 21 ? film.filmName.slice(0, 21)+'...' : film.filmName}
+                    <span data-rm>&#128465</span>
+                </p>
+            `; 
+        }else {
+            parent.innerHTML += `
+            <p>
+                ${index + 1}. 
+                ${film.length >= 21 ? film.slice(0, 21)+'...' : film}
+                <span data-rm>&#128465</span>
+            </p>
+        `; 
+        }
+        });
+        removeFilmFromList("[data-rm]")
+    };
+
+    
+	
+
+
+function removeFilmFromList (selector) {
+	setSort(_DB.movies);
+	document.querySelectorAll(selector).forEach((btn, index) => {
+		btn.addEventListener("click", () => {
+			btn.parentElement.remove();
+			_DB.movies.splice(index, 1);
+			createFilmsList(_DB.movies, filmsBlock);
+		});
+	});
+}
+
+createFilmsList(_DB.movies, filmsBlock);
